b6e414d694823bfad4feb14b92ea147c0eaa0475
Avis <ejerenwaavis@gmailcom>
2021-01-16 23:27:01 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+// const https = require("https");
+// const mongoose = require("mongoose");
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.route("/home")
+  .get(function(req,res){
+    res.render("home");
+  })
+
+
+  app.route("/")
+    .get(function(req,res){
+      res.render("cover.ejs");
+    })
+
+
+
+app.listen(process.env.PORT || 3000, function(){console.log("Paris Hair and Beauty Studio is Live");});
